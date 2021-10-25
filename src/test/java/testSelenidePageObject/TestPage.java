0c9c614ac07320738589219aaa1697d9069e2754
dinosaur321 <dinosaur321@users.com>
2021-10-25 12:38:27 +0300

    Add Check Python code test

--- src/test/java/testSelenidePageObject/TestPage.java
+++ src/test/java/testSelenidePageObject/TestPage.java
@@ -15,7 +15,9 @@ public class TestPage {
 
     private static final String DOCUMENTATION_BUTTON = "//*[@id=\"main_navbar\"]/ul/li[4]/a/span";
     private static final String JAVA_BUTTON = "tabs-1-0-tab";
+    private static final String PYTHON_BUTTON = "tabs-1-1-tab";
     private static final String JAVA_CODE = "//*[@id=\"tabs-1-0\"]/div/div/pre/code";
+    private static final String PYTHON_CODE = "//*[@id=\"tabs-1-1\"]/div/div/pre/code";
     private String EXPECTED_JAVA_CODE = "import org.openqa.selenium.By;\n" +
             "import org.openqa.selenium.Keys;\n" +
             "import org.openqa.selenium.WebDriver;\n" +
@@ -41,18 +43,38 @@ public class TestPage {
             "    }\n" +
             "}\n" +
             "  ";
-
+    private String EXPECTED_PYTHON_CODE = "from selenium import webdriver\n" +
+            "from selenium.webdriver.common.by import By\n" +
+            "from selenium.webdriver.common.keys import Keys\n" +
+            "from selenium.webdriver.support.ui import WebDriverWait\n" +
+            "from selenium.webdriver.support.expected_conditions import presence_of_element_located\n" +
+            "\n" +
+            "#This example requires Selenium WebDriver 3.13 or newer\n" +
+            "with webdriver.Firefox() as driver:\n" +
+            "    wait = WebDriverWait(driver, 10)\n" +
+            "    driver.get(\"https://google.com/ncr\")\n" +
+            "    driver.find_element(By.NAME, \"q\").send_keys(\"cheese\" + Keys.RETURN)\n" +
+            "    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, \"h3\")))\n" +
+            "    print(first_result.get_attribute(\"textContent\"))\n" +
+            "  ";
 
 
     @Test
     public void checkCorrespondCodeTextToJava() {
         open("https://www.selenium.dev/");
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized");
-//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
         $(By.xpath(DOCUMENTATION_BUTTON)).click();
         $(By.id(JAVA_BUTTON)).click();
         $(By.xpath(JAVA_CODE)).shouldHave(text(EXPECTED_JAVA_CODE));
+
+    }
+
+    @Test
+    public void checkCorrespondCodeTextToPython() {
+        open("https://www.selenium.dev/");
+        $(By.xpath(DOCUMENTATION_BUTTON)).click();
+        $(By.id(PYTHON_BUTTON)).click();
+        $(By.xpath(PYTHON_CODE)).shouldHave(text(EXPECTED_PYTHON_CODE));
 
     }
 
